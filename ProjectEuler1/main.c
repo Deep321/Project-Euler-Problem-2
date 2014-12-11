68f5fc374c0e4392dadddb768983faeabcd1ef25
Deep Prasad <Deeptanshu.Prasad@mail.utoronto.ca>
2014-12-10 20:54:05 -0500

    Initial Commit

---
+++ ProjectEuler1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  ProjectEuler1
+//
+//  Created by Deep Prasad on 2014-12-10.
+//  Copyright (c) 2014 Deep Prasad. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}

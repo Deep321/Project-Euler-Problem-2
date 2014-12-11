6e57b2bfca88a93a69908371a0652de2705dcbd5
Deep Prasad <Deeptanshu.Prasad@mail.utoronto.ca>
2014-12-10 21:15:01 -0500

    This is problem 1 of Pro.Euler, done in C

--- ProjectEuler1/main.c
+++ ProjectEuler1/main.c
@@ -1,15 +1,34 @@
 //
 //  main.c
-//  ProjectEuler1
+//  ProjectEuler2
 //
 //  Created by Deep Prasad on 2014-12-10.
 //  Copyright (c) 2014 Deep Prasad. All rights reserved.
 //
 
 #include <stdio.h>
+int fibbo(n)
+{
+    if (fibbo(n)<=1){
+        return 1;
+        }
+    else
+        return fibbo(n-1)+fibbo(n-2);
+    
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+    int sum=0;
+    int i=0;
+    int fibval=0;
+    while(fibval <=4000000)
+    {
+        fibval=fibbo(i);
+        if(fibval%2==0)
+        {
+            sum+=fibval;
+        }
+        i++;
+    }
+    printf("sum: %d",sum);
 }
